0f884990c5a4a44ef52020bee5d7fd250256cb2d
Crowdbotics <team@crowdbotics.com>
2020-09-19 18:09:04 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,1 +1,96 @@
 import axios from "axios"
+const plugin11 = axios.create({
+  baseURL:
+    "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/7307/storyboard/8491/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+const test12312API = axios.create({
+  baseURL: "https://test-12312-dev-11242-prod.herokuapp.com/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_v1_customtext_list() {
+  return test12312API.get(`/api/v1/customtext/`)
+}
+function api_v1_customtext_read() {
+  return test12312API.get(`/api/v1/customtext/{id}/`)
+}
+function api_v1_customtext_update(requestBody) {
+  return test12312API.put(`/api/v1/customtext/{id}/`, requestBody)
+}
+function api_v1_customtext_partial_update(requestBody) {
+  return test12312API.patch(`/api/v1/customtext/{id}/`, requestBody)
+}
+function api_v1_homepage_list() {
+  return test12312API.get(`/api/v1/homepage/`)
+}
+function api_v1_homepage_read() {
+  return test12312API.get(`/api/v1/homepage/{id}/`)
+}
+function api_v1_homepage_update(requestBody) {
+  return test12312API.put(`/api/v1/homepage/{id}/`, requestBody)
+}
+function api_v1_homepage_partial_update(requestBody) {
+  return test12312API.patch(`/api/v1/homepage/{id}/`, requestBody)
+}
+function api_v1_login_create() {
+  return test12312API.post(`/api/v1/login/`)
+}
+function api_v1_signup_create(requestBody) {
+  return test12312API.post(`/api/v1/signup/`, requestBody)
+}
+function rest_auth_login_create(requestBody) {
+  return test12312API.post(`/rest-auth/login/`, requestBody)
+}
+function rest_auth_logout_list() {
+  return test12312API.get(`/rest-auth/logout/`)
+}
+function rest_auth_logout_create() {
+  return test12312API.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(requestBody) {
+  return test12312API.post(`/rest-auth/password/change/`, requestBody)
+}
+function rest_auth_password_reset_create(requestBody) {
+  return test12312API.post(`/rest-auth/password/reset/`, requestBody)
+}
+function rest_auth_password_reset_confirm_create(requestBody) {
+  return test12312API.post(`/rest-auth/password/reset/confirm/`, requestBody)
+}
+function rest_auth_registration_create(requestBody) {
+  return test12312API.post(`/rest-auth/registration/`, requestBody)
+}
+function rest_auth_registration_verify_email_create(requestBody) {
+  return test12312API.post(`/rest-auth/registration/verify-email/`, requestBody)
+}
+function rest_auth_user_read() {
+  return test12312API.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(requestBody) {
+  return test12312API.put(`/rest-auth/user/`, requestBody)
+}
+function rest_auth_user_partial_update(requestBody) {
+  return test12312API.patch(`/rest-auth/user/`, requestBody)
+}
+export const apiService = {
+  api_v1_customtext_list,
+  api_v1_customtext_read,
+  api_v1_customtext_update,
+  api_v1_customtext_partial_update,
+  api_v1_homepage_list,
+  api_v1_homepage_read,
+  api_v1_homepage_update,
+  api_v1_homepage_partial_update,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_list,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_read,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
+}
